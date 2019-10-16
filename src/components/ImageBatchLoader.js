0a04046dff32248ed0b84a2fd3a898652cd0d502
oleksandrbyk <obyk320@gmail.com>
2019-10-16 06:27:46 +0300

    specified npm and node version and some fix

--- src/components/ImageBatchLoader.js
+++ src/components/ImageBatchLoader.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Container, Row, Col, Spinner } from 'react-bootstrap'
-import './ImageBatchLoader.css'
-import ReactCrop from 'react-image-crop';
+import { Container, Row, Col, Spinner } from "react-bootstrap";
+import "./ImageBatchLoader.css";
+import ReactCrop from "react-image-crop";
 import "react-image-crop/dist/ReactCrop.css";
-import * as faceapi from 'face-api.js';
-import _ from 'lodash';
+import * as faceapi from "face-api.js";
+import _ from "lodash";
 
 class ImageBatchLoader extends React.Component {
   constructor(props) {
@@ -20,11 +20,13 @@ class ImageBatchLoader extends React.Component {
     };
   }
 
-  filesUploader = (files) => {
-    const maxSize = 4000000, minSize = 50000
-    for(let i = 0; i < files.length; i++) {
-      if(files[i].size > maxSize || files[i].size < minSize) {
-        const error = 'Please pick the files what sizes are between 50000 byte and 4000000 byte\n'
+  filesUploader = files => {
+    const maxSize = 4000000,
+      minSize = 50000;
+    for (let i = 0; i < files.length; i++) {
+      if (files[i].size > maxSize || files[i].size < minSize) {
+        const error =
+          "Please pick the files what sizes are between 50000 byte and 4000000 byte\n";
         console.log(error);
         return;
       }
@@ -35,44 +37,45 @@ class ImageBatchLoader extends React.Component {
       currentFileIndex: 0,
       isDetectedFace: false,
       src: Array.from(files).map(file => URL.createObjectURL(file))
-    })
-  }
+    });
+  };
 
   setCrop = newCrop => {
-    const { currentFileIndex } = this.state
-    let { crop } = this.state
-    let currentCrop = crop[currentFileIndex]
-    if(!_.isEqual(newCrop, currentCrop)) {
-      crop[currentFileIndex] = Object.assign({}, currentCrop, newCrop)
-      this.setState({ crop })
+    const { currentFileIndex } = this.state;
+    let { crop } = this.state;
+    let currentCrop = crop[currentFileIndex];
+    if (!_.isEqual(newCrop, currentCrop)) {
+      crop[currentFileIndex] = Object.assign({}, currentCrop, newCrop);
+      this.setState({ crop });
     }
-  }
+  };
 
   onImageLoaded = async image => {
-    const { croppedImages, currentFileIndex } = this.state
+    const { croppedImages, currentFileIndex } = this.state;
     this.imageRef = image;
     if (croppedImages[currentFileIndex]) {
-      this.setState({ isDetectedFace: true })
+      this.setState({ isDetectedFace: true });
     } else {
-      const { naturalWidth, naturalHeight, width, height } = image
-      await faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
-      const { box } = await faceapi.detectSingleFace(image)
+      const { naturalWidth, naturalHeight, width, height } = image;
+      await faceapi.nets.ssdMobilenetv1.loadFromUri("/models");
+      const { box } = await faceapi.detectSingleFace(image);
       // In general, rateX is equal to rateY
-      const rateX = width / naturalWidth, rateY = height / naturalHeight;
+      const rateX = width / naturalWidth,
+        rateY = height / naturalHeight;
 
-      this.setState({ isDetectedFace: true })
+      this.setState({ isDetectedFace: true });
       this.setCrop({
         x: box._x * rateX,
         y: box._y * rateY,
         width: box._width * rateX,
         height: box._height * rateY
-      })
+      });
     }
-  }
+  };
 
   saveCroppedImage = async () => {
-    const { crop, currentFileIndex } = this.state
-    const _crop = crop[currentFileIndex]
+    const { crop, currentFileIndex } = this.state;
+    const _crop = crop[currentFileIndex];
 
     if (this.imageRef && _crop.width && _crop.height) {
       const croppedImage = await this.getCroppedImg(
@@ -80,14 +83,14 @@ class ImageBatchLoader extends React.Component {
         _crop,
         "newFile.jpeg"
       );
-      let { croppedImages } = this.state
-      croppedImages[currentFileIndex] = croppedImage
-      this.setState({ croppedImages })
+      let { croppedImages } = this.state;
+      croppedImages[currentFileIndex] = croppedImage;
+      this.setState({ croppedImages });
     }
-  }
+  };
 
   getCroppedImg(image, crop, fileName) {
-    const { resolution } = this.state
+    const { resolution } = this.state;
     const canvas = document.createElement("canvas");
     const scaleX = image.naturalWidth / image.width;
     const scaleY = image.naturalHeight / image.height;
@@ -108,17 +111,21 @@ class ImageBatchLoader extends React.Component {
     );
 
     return new Promise((resolve, reject) => {
-      canvas.toBlob(blob => {
-        if (!blob) {
-          //reject(new Error('Canvas is empty'));
-          console.error("Canvas is empty");
-          return;
-        }
-        blob.name = fileName;
-        // window.URL.revokeObjectURL(this.fileUrl);
-        this.fileUrl = window.URL.createObjectURL(blob);
-        resolve(this.fileUrl);
-      }, "image/jpeg", resolution * 1);
+      canvas.toBlob(
+        blob => {
+          if (!blob) {
+            //reject(new Error('Canvas is empty'));
+            console.error("Canvas is empty");
+            return;
+          }
+          blob.name = fileName;
+          // window.URL.revokeObjectURL(this.fileUrl);
+          this.fileUrl = window.URL.createObjectURL(blob);
+          resolve(this.fileUrl);
+        },
+        "image/jpeg",
+        resolution * 1
+      );
     });
   }
 
@@ -132,14 +139,16 @@ class ImageBatchLoader extends React.Component {
       crop
     } = this.state;
 
-   return (
+    return (
       <Container>
         {/* Begin - Image Files Loader */}
         <Row>
           <Col>
             <div className="input-group mb-3">
               <div className="input-group-prepend">
-                <span className="input-group-text" id="inputGroupFileAddon01">Upload</span>
+                <span className="input-group-text" id="inputGroupFileAddon01">
+                  Upload
+                </span>
               </div>
               <div className="custom-file">
                 <input
@@ -162,7 +171,9 @@ class ImageBatchLoader extends React.Component {
               type="number"
               className="form-control"
               value={crop[currentFileIndex] ? crop[currentFileIndex].width : 0}
-              onChange={e => { this.setCrop({ width: e.currentTarget.value - 0 }) }}
+              onChange={e => {
+                this.setCrop({ width: e.currentTarget.value - 0 });
+              }}
             />
           </Col>
           <Col sm={3}>
@@ -171,7 +182,9 @@ class ImageBatchLoader extends React.Component {
               type="number"
               className="form-control"
               value={crop[currentFileIndex] ? crop[currentFileIndex].height : 0}
-              onChange={e => { this.setCrop({ height: e.currentTarget.value - 0 }) }}
+              onChange={e => {
+                this.setCrop({ height: e.currentTarget.value - 0 });
+              }}
             />
           </Col>
           <Col sm={6}>
@@ -194,7 +207,12 @@ class ImageBatchLoader extends React.Component {
         {/* Begin - "Single Crop" Button */}
         <Row>
           <Col>
-            <input type="button" className="btn btn-primary btn-block" value="Single Crop" onClick={this.saveCroppedImage} />
+            <input
+              type="button"
+              className="btn btn-primary btn-block"
+              value="Single Crop"
+              onClick={this.saveCroppedImage}
+            />
           </Col>
         </Row>
         {/* End - "Single Crop" Button */}
@@ -207,7 +225,7 @@ class ImageBatchLoader extends React.Component {
               onClick={() => {
                 this.setState({
                   currentFileIndex: currentFileIndex - 1,
-                  isDetectedFace: false,
+                  isDetectedFace: false
                 });
               }}
               disabled={currentFileIndex <= 0}
@@ -220,7 +238,7 @@ class ImageBatchLoader extends React.Component {
               onClick={() => {
                 this.setState({
                   currentFileIndex: currentFileIndex + 1,
-                  isDetectedFace: false,
+                  isDetectedFace: false
                 });
               }}
               disabled={currentFileIndex === files.length - 1}
@@ -244,7 +262,10 @@ class ImageBatchLoader extends React.Component {
         {/* End - react-image-crop */}
         {/* Begin - Preview cropped image */}
         <Row>
-          <img id="overlay" src={croppedImages.length ? croppedImages[currentFileIndex] : null } />
+          <img
+            id="overlay"
+            src={croppedImages.length ? croppedImages[currentFileIndex] : null}
+          />
         </Row>
         {/* End - Preview cropped image */}
       </Container>
